2dab2703875c9e80802b3f63561cd8967e19c8ea
Paphawit Ngamchaliew <greatng@Paphawits-MacBook-Air.local>
2022-05-23 10:39:00 +0700

    add builtin 2

--- src/builtin_2.c
+++ src/builtin_2.c
@@ -6,7 +6,7 @@
 /*   By: pngamcha <pngamcha@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 10:17:34 by pngamcha          #+#    #+#             */
-/*   Updated: 2022/05/23 10:18:10 by pngamcha         ###   ########.fr       */
+/*   Updated: 2022/05/23 10:28:15 by pngamcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ extern	t_mini	g_mini;
 
 void	print_env(void)
 {
-	for (int i = 0; g_mini.env[i]; i++)
-		printf("%s\n", g_mini.env[i]);
+	int	i;
+	
+	i = 0;
+	while (g_mini.env[i])
+		printf("%s\n", g_mini.env[i++]);
 }
