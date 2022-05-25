cdc7858e73fea596b3ba52db649ea14714e1414c
Paphawit Ngamchaliew <paphawit.ng@gmail.com>
2022-05-26 00:51:30 +0700

    add signal

--- src/signal.c
+++ src/signal.c
@@ -6,7 +6,7 @@
 /*   By: pngamcha <pngamcha@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 14:49:20 by pngamcha          #+#    #+#             */
-/*   Updated: 2022/05/24 11:54:14 by pngamcha         ###   ########.fr       */
+/*   Updated: 2022/05/26 00:50:57 by pngamcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 static void	shell_sighandler(int sig)
 {
 	if (sig == SIGINT)
+	{
 		rl_on_new_line();
+		rl_redisplay();
+	}
 	if (sig == SIGQUIT)
 		return ;
 }
@@ -26,7 +29,7 @@ void	shell_signal(void)
 
 	sa.sa_handler = shell_sighandler;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
+	sa.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa, NULL);
 	sigaction(SIGQUIT, &sa, NULL);
 }
